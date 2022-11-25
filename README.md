c565e7a6ba2f5325e160b6939dbabcbbdfd2e5fd
Kalvium-Program <109285740+Kalvium-Program@users.noreply.github.com>
2022-11-25 12:23:18 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-![Logo-nav](https://user-images.githubusercontent.com/109285740/201738412-c7836450-7642-471d-8b7c-92b8f8ea62f0.png)
-
+![Logo-nav](https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png)
 
 # Kalvium Project-Builder | JS | LIVE NEWS
 
 We have learned all the important concepts in JavaScript. Now lets work with our project builder in building a smartwatch, applying all of the concepts we have just learned.
 
-### Progression 1: GET MY API
-Create an account in (https://newsapi.org/). Once you create an account, go to settings and get your API key.
+## How to approach this lab:
 
-### Progression 2: INDIA NEWS-LIVE
-Use Axios to fetch the data from https://newsapi.org/ api and display it as shown in the output.
-![Image description](https://i1.faceprep.in/ProGrad/news-app2.JPG) 
+There are no test cases for this lab - you just need to complete the task given below and compare the your output with the image provided below.
+As there are no test cases - so you are free to explore and experiment with your frontend skills -- keeping in mind -> the basic and required functionality mentioned below **must** be achieved -- only after that you are free to show-off your frontend skills!
 
-### Progression 3: US NEWS-LIVE
-Show the top most news in US
-![Image description](https://i1.faceprep.in/ProGrad/news-app1.JPG)
+### To Do: GET MY API
 
+Create an account in (https://gnews.io/). Once you create an account, go to settings and get your API key.
 
+### Progression 1: INDIA NEWS-LIVE
+
+Use Axios to fetch the data from https://gnews.io/ api and display it as shown in the output.
+![Image description](https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/js-live-news.png)
 
 Happy Coding Kalvium❤️
--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 let arr = [];
-function getData(){
 
-  const getBlog = document.getElementById("blog");
+// Progression 1: create a function and fetch the api using axios
+function getData() {
+  const getBlog = document.getElementById('blog');
 
-  axios.get("https://gnews.io/api/v4/search?q=example&token=20272535ca1f753dbf1a5dae4438eca8&lang=en").
-  then(res=>{
+  axios
+    .get(
+      'https://gnews.io/api/v4/search?q=example&token=20272535ca1f753dbf1a5dae4438eca8&lang=en'
+    )
+    .then((res) => {
+      const listOfArticles = res.data.articles;
 
-    const listOfArticles = res.data.articles;
+      listOfArticles.forEach((article) => {
+        const getArticle = document.createElement('div');
+        getArticle.setAttribute('id', 'article');
+        getArticle.setAttribute('class', 'article');
 
-    listOfArticles.forEach((article) => {
+        const title = document.createElement('h3');
+        const content = document.createElement('p');
+        const img = document.createElement('img');
 
-      const getArticle = document.createElement("div");
-      getArticle.setAttribute("id","article");
-      getArticle.setAttribute("class","article");
+        title.innerText = article.title;
+        content.innerText = article.content;
+        img.setAttribute('src', article.image);
 
+        getArticle.append(title);
+        getArticle.append(img);
+        getArticle.append(content);
 
-      const title = document.createElement("h3");
-      const content = document.createElement("p")
-      const img = document.createElement("img");
+        arr.push(getArticle);
+      });
 
-      title.innerText = article.title;
-      content.innerText = article.content;
-      img.setAttribute("src",article.image);
-
-      getArticle.append(title)
-      getArticle.append(img);
-      getArticle.append(content);
-
-      arr.push(getArticle);
-
-      
-
-      
+      arr.forEach((ele) => {
+        getBlog.append(ele);
+      });
     });
-
-    arr.forEach(ele=>{
-      getBlog.append(ele);
-    })
-
-
-    
-  })
-};
+}
 
 getData();
